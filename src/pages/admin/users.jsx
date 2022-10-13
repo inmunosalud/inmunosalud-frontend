413fef3f0b6c773e56fd22be8bcfe0f5962ce243
droblesvd <drobles@vainilladev.com>
2022-10-12 21:11:15 -0500

    hot fix

--- src/pages/admin/users.jsx
+++ src/pages/admin/users.jsx
@@ -1,8 +1,12 @@
 // ** MUI Imports
-import Grid from '@mui/material/Grid'
 
 // ** Styled Component Import
 import ApexChartWrapper from 'src/@core/styles/libs/react-apexcharts'
+
+import PageHeader from 'src/@core/components/page-header'
+import Typography from '@mui/material/Typography'
+import Grid from '@mui/material/Grid'
+import { Button, Card } from '@mui/material'
 
 import NumberUsers from 'src/views/general/NumberUsers'
 import CardNumber from 'src/views/general/CardNumber'
@@ -30,6 +34,20 @@ const General = () => {
   return (
     <ApexChartWrapper>
       <Grid container spacing={6}>
+        <PageHeader title={<Typography variant='h5'>Productos</Typography>} />
+
+        <Grid item display='flex' justifyContent='flex-end' xs={12}>
+          <Button
+            variant='contained'
+            sx={{ mb: 3, whiteSpace: 'nowrap' }}
+            onClick={() => {
+              handleClickOpen()
+              setDialogTitle('Add')
+            }}
+          >
+            Alta de producto
+          </Button>
+        </Grid>
         <Grid item xs={12}>
           <TableUsers />
         </Grid>
--- src/pages/ecommerce/products.js
+++ src/pages/ecommerce/products.js
@@ -3,7 +3,6 @@ import Grid from '@mui/material/Grid'
 import Typography from '@mui/material/Typography'
 
 import PageHeader from 'src/@core/components/page-header'
-import { AddProduct } from 'src/views/dashboards/products/addProduct'
 import { ProductItem } from 'src/views/dashboards/products/ProductItem'
 
 import TableUsers from 'src/views/dashboards/users/TableUsers'
