8885138fd951624ef858b66b1489c09b56a58c23
Alan Anaya <hello@alananayadev.com>
2023-05-11 23:10:47 -0600

    feat: constants section creation

---
+++ src/pages/dashboards/constants.js
@@ -0,0 +1,19 @@
+import { CardHeader, Container, Divider, Grid, Card, CardContent } from '@mui/material'
+
+const Constants = () => {
+  return (
+    <>
+      <Card>
+        <CardHeader title={'Editar constantes del sistema'} titleTypographyProps={{ variant: 'h6' }} />
+        <Grid item xs={12}>
+          <Divider />
+        </Grid>
+        <CardContent>
+            
+        </CardContent>
+      </Card>
+    </>
+  )
+}
+
+export default Constants
