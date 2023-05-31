e3ce27b185f0ed71342a5cba6db42a8a1970013d
Diego Martin Ponce Villalvazo <58534750+TheDMPV@users.noreply.github.com>
2023-05-30 19:46:18 -0600

    corrección de color

--- src/layouts/UserThemeOptions.js
+++ src/layouts/UserThemeOptions.js
@@ -1,23 +1,17 @@
 // ** To use core palette, uncomment the below import
-// import corePalette from 'src/@core/theme/palette'
+import corePalette from 'src/@core/theme/palette'
 // ** To use mode (light/dark), skin(default/bordered/semi-dark), direction(ltr/rtl), etc. for conditional styles, uncomment below line
-// import { useSettings } from 'src/@core/hooks/useSettings'
+import { useSettings } from 'src/@core/hooks/useSettings'
 const UserThemeOptions = () => {
   // ** To use mode (light/dark), skin(default/bordered/semi-dark), direction(ltr/rtl), etc. for conditional styles, uncomment below line
-  // const { settings } = useSettings()
+  const { settings } = useSettings()
   // ** To use mode (light/dark), skin(default/bordered/semi-dark), direction(ltr/rtl), etc. for conditional styles, uncomment below line
-  // const { mode, skin, themeColor } = settings
+  const { mode, skin, themeColor } = settings
   // ** To use core palette, uncomment the below line
-  // const palette = corePalette(mode, skin, themeColor)
+  const palette = corePalette(mode, skin, themeColor)
+
   return {
-    palette: {
-      primary: {
-        light: '#00ABF5',
-        main: '#0091CD',
-        dark: '#0082BA',
-        contrastText: '#FFF'
-      }
-    }
+    palette,
 
     /*
     /*
