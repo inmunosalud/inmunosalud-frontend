84c69079d20cedadee667541022a8501671973ae
Diego Ponce <58534750+TheDMPV@users.noreply.github.com>
2024-10-31 16:44:12 -0600

    go back button fix

--- src/views/components/goback/GoBack.js
+++ src/views/components/goback/GoBack.js
@@ -1,13 +1,15 @@
 // import * as React from 'react'
 import Button from '@mui/material/Button'
 import ArrowLeft from 'mdi-material-ui/ArrowLeft'
-
+import Link from 'next/link'
 export const GoBackButton = ({ onChangePage }) => {
   return (
     <div style={{ margin: '8px', witdh: '8px', height: '8px' }}>
-      <Button style={{ borderRadius: '30px' }} variant='contained' onClick={onChangePage}>
-        <ArrowLeft />
-      </Button>
+      <Link href='/landing-page/home' passHref style={{ textDecoration: 'none' }}>
+        <Button style={{ borderRadius: '30px' }} variant='contained'>
+          <ArrowLeft />
+        </Button>
+      </Link>
     </div>
   )
 }
