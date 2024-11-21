41e20f809223714555ae936e7854723a3f56a170
Diego Ponce <die_mar.11@hotmail.com>
2024-11-21 12:50:03 -0600

    Update FooterContent.js

--- src/@core/layouts/components/shared-components/footer/FooterContent.js
+++ src/@core/layouts/components/shared-components/footer/FooterContent.js
@@ -22,12 +22,6 @@ const FooterContent = () => {
     >
       <Box display='flex' flexDirection='column'>
         <Box display='flex' alignItems='center' gap={1}>
-          <IconButton color='primary' href='https://wa.me/523334173934' target='_blank' rel='noopener noreferrer'>
-            <WhatsApp />
-            <Typography fontSize='12px'>
-              <strong>+52 3334173934</strong>
-            </Typography>
-          </IconButton>
           <IconButton
             color='primary'
             href='https://www.facebook.com/inmunosaludgdl'
@@ -43,6 +37,12 @@ const FooterContent = () => {
             rel='noopener noreferrer'
           >
             <Instagram />
+          </IconButton>
+          <IconButton color='primary' href='https://wa.me/523334173934' target='_blank' rel='noopener noreferrer'>
+            <WhatsApp />
+            <Typography sx={{ ml: 1 }} fontSize='12px' fontWeight='bold'>
+              {'+52 33 3417 3934'}
+            </Typography>
           </IconButton>
         </Box>
       </Box>
