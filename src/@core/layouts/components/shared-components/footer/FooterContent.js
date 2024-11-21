165d28b96b25f06619c277fdf1f6e6db453250c8
Diego Ponce <die_mar.11@hotmail.com>
2024-11-21 12:37:59 -0600

    Update FooterContent.js

--- src/@core/layouts/components/shared-components/footer/FooterContent.js
+++ src/@core/layouts/components/shared-components/footer/FooterContent.js
@@ -2,24 +2,54 @@
 import Box from '@mui/material/Box'
 import Link from '@mui/material/Link'
 import Typography from '@mui/material/Typography'
+import IconButton from '@mui/material/IconButton'
 import useMediaQuery from '@mui/material/useMediaQuery'
-import { Whatsapp } from 'mdi-material-ui'
+import { WhatsApp, Facebook, Instagram } from '@mui/icons-material'
 
 const FooterContent = () => {
   // ** Var
   const hidden = useMediaQuery(theme => theme.breakpoints.down('md'))
 
   return (
-    <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'space-between' }}>
+    <Box
+      sx={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: 2
+      }}
+    >
       <Box display='flex' flexDirection='column'>
-        <Box display='flex' alignItems='center'>
-          <Whatsapp color='primary' />
-          <Typography fontSize={'12px'}>
-            <strong>+52 3334173934</strong>
-          </Typography>
+        <Box display='flex' alignItems='center' gap={1}>
+          <IconButton color='primary' href='https://wa.me/523334173934' target='_blank' rel='noopener noreferrer'>
+            <WhatsApp />
+            <Typography fontSize='12px'>
+              <strong>+52 3334173934</strong>
+            </Typography>
+          </IconButton>
+          <IconButton
+            color='primary'
+            href='https://www.facebook.com/inmunosaludgdl'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <Facebook />
+          </IconButton>
+          <IconButton
+            color='primary'
+            href='https://www.instagram.com/inmunosaludgdl'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <Instagram />
+          </IconButton>
         </Box>
       </Box>
-      <Typography>Inmunosalud</Typography>
+      <Typography fontSize='14px' fontWeight='bold'>
+        Inmunosalud
+      </Typography>
+      <Box display='flex' gap={1}></Box>
     </Box>
   )
 }
