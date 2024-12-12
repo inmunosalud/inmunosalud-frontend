61062c454950a519b1a236905b442f1504d0fd84
Diego Ponce <die_mar.11@hotmail.com>
2024-12-12 16:58:17 -0600

    Update NetworkUsers.jsx

--- src/views/pages/network/NetworkUsers.jsx
+++ src/views/pages/network/NetworkUsers.jsx
@@ -20,7 +20,7 @@ export const NetworkUsers = () => {
       <CardContent>
         <Grid container spacing={2}>
           <Grid item xs={12} md={12}>
-            <CardHeader title='Usuarios en tu red' />
+            <CardHeader title={`Usuarios en tu red: ${network.network?.totalUsers || '0'}`} />
           </Grid>
 
           <Grid item xs={12} md={12}>
@@ -29,65 +29,65 @@ export const NetworkUsers = () => {
                 textAlign: 'center'
               }}
             >
-              <Typography variant='h6' color='textPrimary'>
-                <strong>
-                  {' '}
-                  Total de usuarios en tu red:{' '}
-                  <span style={{ color: theme.palette.primary.main }}> {network.network?.totalUsers || '0'} </span>{' '}
-                </strong>
-              </Typography>
-
               <Grid container spacing={2}>
                 <Grid item xs={12} sm={3}>
-                  <h2>
-                    Nivel 1:{' '}
-                    <span
-                      style={{
-                        color: theme.palette.primary.main
-                      }}
-                    >
-                      {network?.network?.['1']?.length || 0}
-                    </span>
-                  </h2>
+                  <Typography fontWeight='bold' variant='h5'>
+                    Nivel 1
+                  </Typography>
+                  <Typography variant='body2' color='textPrimary'>
+                    <b>Usuarios:</b> {network?.network?.['1']?.length || 0}
+                  </Typography>
+                  <Typography variant='body2' color='textPrimary'>
+                    <b>Consumo:</b> ${network?.network?.totalConsumptions?.['1'] || 0}
+                  </Typography>
+                  <Typography sx={{ mb: 1 }} variant='body2' color='textPrimary'>
+                    <b>Comision:</b> ${network?.network?.totalCommissions?.['1'] || 0}
+                  </Typography>
                   {network?.network?.['1'] && <NetworkList firstLevel={true} users={network?.network?.['1']} />}
                 </Grid>
                 <Grid item xs={12} sm={3}>
-                  <h2>
-                    Nivel 2:{' '}
-                    <span
-                      style={{
-                        color: theme.palette.primary.main
-                      }}
-                    >
-                      {network?.network?.['2']?.length || 0}
-                    </span>
-                  </h2>
+                  <Typography fontWeight='bold' variant='h5'>
+                    Nivel 2
+                  </Typography>
+                  <Typography variant='body2' color='textPrimary'>
+                    <b>Usuarios:</b> {network?.network?.['2']?.length || 0}
+                  </Typography>
+                  <Typography variant='body2' color='textPrimary'>
+                    <b>Consumo:</b> ${network?.network?.totalConsumptions?.['2'] || 0}
+                  </Typography>
+                  <Typography sx={{ mb: 2 }} variant='body2' color='textPrimary'>
+                    <b>Comision:</b> ${network?.network?.totalCommissions?.['2'] || 0}
+                  </Typography>
                   {network?.network?.['2'] && <NetworkList users={network?.network?.['2']} />}
                 </Grid>
                 <Grid item xs={12} sm={3}>
-                  <h2>
-                    Nivel 3:{' '}
-                    <span
-                      style={{
-                        color: theme.palette.primary.main
-                      }}
-                    >
-                      {network?.network?.['3']?.length || 0}{' '}
-                    </span>
-                  </h2>
+                  <Typography fontWeight='bold' variant='h5'>
+                    Nivel 3
+                  </Typography>
+                  <Typography variant='body2' color='textPrimary'>
+                    <b>Usuarios:</b> {network?.network?.['3']?.length || 0}
+                  </Typography>
+                  <Typography variant='body2' color='textPrimary'>
+                    <b>Consumo:</b> ${network?.network?.totalConsumptions?.['3'] || 0}
+                  </Typography>
+                  <Typography sx={{ mb: 3 }} variant='body2' color='textPrimary'>
+                    <b>Comision:</b> ${network?.network?.totalCommissions?.['3'] || 0}
+                  </Typography>
                   {network?.network?.['3'] && <NetworkList users={network?.network?.['3']} />}
                 </Grid>
                 <Grid item xs={12} sm={3}>
-                  <h2>
-                    Nivel 4:{' '}
-                    <span
-                      style={{
-                        color: theme.palette.primary.main
-                      }}
-                    >
-                      {network?.network?.['4']?.length || 0}{' '}
-                    </span>
-                  </h2>
+                  <Typography fontWeight='bold' variant='h5'>
+                    Nivel 4
+                  </Typography>
+                  <Typography variant='body2' color='textPrimary'>
+                    <b>Usuarios:</b> {network?.network?.['4']?.length || 0}
+                  </Typography>
+                  <Typography variant='body2' color='textPrimary'>
+                    <b>Consumo:</b> ${network?.network?.totalConsumptions?.['4'] || 0}
+                  </Typography>
+                  <Typography sx={{ mb: 4 }} variant='body2' color='textPrimary'>
+                    <b>Comision:</b> ${network?.network?.totalCommissions?.['4'] || 0}
+                  </Typography>
                   {network?.network?.['4'] && <NetworkList users={network?.network?.['4']} />}
                 </Grid>
               </Grid>
